a489cbfaa073d78e52a258cb54b5393f33329eb4
Rio <riopappu053@gmail.com>
2026-08-18 10:41:55 +0530

    Delete print_style_block.js

--- print_style_block.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('login.html', 'utf8');
-const lines = content.split('\n');
-for (let i = 210; i < 300; i++) {
-  console.log(`${i+1}: ${lines[i]}`);
-}
